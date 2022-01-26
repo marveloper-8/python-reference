0b19cbb92d6ba8005371f02d3f91efbc084bb3ce
Joshua Equere <marveloper.8@gmail.com>
2022-01-26 19:55:41 +0100

    add a tuples data to a list using the extend function

--- 3.py
+++ 3.py
@@ -1,6 +1,7 @@
 a = ["apple", "banana", "cherry"]
 b = ["Ford", "BMW", "Volvo"]
 c = [1, 4, 2, 9, 7, 8, 9, 3, 1]
+e = (1, 4, 5, 9)
 
 d = a.copy()
 print(d)
@@ -9,8 +10,11 @@ print(a.count("cherry"))
 
 print(c.count(9))
 
-a.extend(b)
+a.extend(e)
 print(a)
+
+# a.extend(b)
+# print(a)
 
 # a.append("orange")
 # print(a)
