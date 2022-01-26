7f9b62c71e55d731a22667ef91de2a6b298711a7
Joshua Equere <marveloper.8@gmail.com>
2022-01-26 19:49:33 +0100

    get the number of times a value appears in a list

--- 3.py
+++ 3.py
@@ -1,10 +1,16 @@
 a = ["apple", "banana", "cherry"]
-a.append("orange")
-print(a)
 
-b = ["Ford", "BMW", "Volvo"]
-a.append(b)
-print(a)
+b = a.copy()
+print(b)
 
-a.clear()
-print(a)
+print(a.count("cherry"))
+
+# a.append("orange")
+# print(a)
+
+# b = ["Ford", "BMW", "Volvo"]
+# a.append(b)
+# print(a)
+
+# a.clear()
+# print(a)
