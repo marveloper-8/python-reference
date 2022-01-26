1ba01a121a89306fc98cf0cd887f152dac9c3933
Joshua Equere <marveloper.8@gmail.com>
2022-01-26 13:50:55 +0100

    casefold is similar to lower but more aggressive. it is to lower all the characters in a sentence

--- 2.py
+++ 2.py
@@ -1,3 +1,7 @@
+# capitalize
 print("hello, and welcome to my world.".capitalize())
-
 print("python is FUN!".capitalize())
+print("36 is my age.".capitalize())
+
+# casefold
+print("Hello, And Welcome To My World!".casefold())
