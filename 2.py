e2a1f5dee82d427ac44f48a93be3f351d5149041
Joshua Equere <marveloper.8@gmail.com>
2022-01-26 17:29:54 +0100

    split each word in a string and turn it into a list

--- 2.py
+++ 2.py
@@ -130,3 +130,6 @@ print("hello, welcome to my world.".startswith("wel", 7, 20))
 # splitlines
 print("Thank you for the music\nWelcome to the jungle".splitlines())
 print("Thank you for the music\nWelcome to the jungle".splitlines(True))
+
+# split
+print("welcome to the jungle".split())
