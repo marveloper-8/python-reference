125e5ea8cc1c1fc4b34727aac6c1f339f88abe63
Joshua Equere <marveloper.8@gmail.com>
2022-01-26 17:51:30 +0100

    split a string into list using a particular set of characters as the separator starting from the right

--- 2.py
+++ 2.py
@@ -140,3 +140,6 @@ print("apple#banana#cherry#orange".split("#", 1))
 # rstrip
 print("of all fruits", "     banana     ".rstrip(), "is my favourite")
 print("banana,,,,,ssqqqww.....".rstrip(",.qsw"))
+
+# rsplit
+print("apple, banana, cherry".rsplit(", "))
