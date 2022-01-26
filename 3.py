ac9a3beffe9cdcb6d372b518014c4546171e8455
Joshua Equere <marveloper.8@gmail.com>
2022-01-26 20:02:10 +0100

    insert element into a particular position on the list

--- 3.py
+++ 3.py
@@ -2,18 +2,23 @@ a = ["apple", "banana", "cherry"]
 b = ["Ford", "BMW", "Volvo"]
 c = [1, 4, 2, 9, 7, 8, 9, 3, 1]
 e = (1, 4, 5, 9)
+f = [4, 55, 64, 32, 16, 32]
 
-d = a.copy()
-print(d)
-
-print(a.count("cherry"))
-
-print(c.count(9))
-
-a.extend(e)
+a.insert(1, "orange")
 print(a)
 
-print(a.index("cherry"))
+# d = a.copy()
+# print(d)
+
+# print(a.count("cherry"))
+
+# print(c.count(9))
+
+# a.extend(e)
+# print(a)
+
+# print(a.index("cherry"))
+# print(f.index(32))
 
 # a.extend(b)
 # print(a)
