d7872b91bdde6ba0ca8083e8658da981515ef898
Joshua Equere <marveloper.8@gmail.com>
2022-01-26 13:59:10 +0100

    count returns the number of times a specified value appears in a string

--- 2.py
+++ 2.py
@@ -9,3 +9,6 @@ print("Hello, And Welcome To My World!".casefold())
 # center
 print("banana".center(20))
 print("banana".center(20, "O"))
+
+# count
+print("I love apples, apple are my favourite fruit".count("apple"))
