ea207ec0466bb8a3d4f8ab1fb5395cf244cb20c9
Joshua Equere <marveloper.8@gmail.com>
2022-01-27 17:11:12 +0100

    flush method cleans out the internal buffer

--- 7.py
+++ 7.py
@@ -3,4 +3,9 @@ a = open("demofile.txt", "r")
 # print(a.read())
 # a.close()
 
-print(a.fileno())
+# print(a.fileno())
+
+b = open("myfile.txt", "a")
+b.write("Now the file has one more line!")
+b.flush()
+b.write("...add another one!")
