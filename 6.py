2cb00d8f9ee0b703800d8ff4b0d71109c983ad6b
Joshua Equere <marveloper.8@gmail.com>
2022-01-27 07:52:56 +0100

    create a copy of a set

--- 6.py
+++ 6.py
@@ -1,10 +1,13 @@
 a = {"apple", "banana", "cherry"}
 
 # add
-a.add("orange")
-a.add("apple")
-print(a)
+# a.add("orange")
+# a.add("apple")
+# print(a)
 
 # clear
-a.clear()
-print(a)
+# a.clear()
+# print(a)
+
+# copy
+print(a.copy())
