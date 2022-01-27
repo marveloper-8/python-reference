0145e2f6478cebc41526991b95a9b0bbfec2fe53
Joshua Equere <marveloper.8@gmail.com>
2022-01-27 20:47:18 +0100

    as is used to create an alias

--- 8.py
+++ 8.py
@@ -1,7 +1,11 @@
+import calendar as c
+
 # and
 print((5 > 3 and 5 < 10))
-
 if 5 > 3 and 5 < 10:
     print("Both statements are True")
 else:
     print("At least one of the statements are False")
+
+# as
+print(c.month_name[1])
