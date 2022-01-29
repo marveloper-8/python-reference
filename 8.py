d9e6a928c6bcc68cb7eb82d41a5c6d37d3a55330
Joshua Equere <marveloper.8@gmail.com>
2022-01-29 03:44:31 +0100

    use the or operator in an if statement

--- 8.py
+++ 8.py
@@ -230,3 +230,8 @@ print(not False)
 
 # or
 print((5 > 3 or 5 > 10))
+
+if 5 > 3 or 5 > 10:
+    print("At least one of the statements are True")
+else:
+    print("None of the statements are True")
