d6266b9c620ccac6a75bc269d0b519dc6929adab
Joshua Equere <marveloper.8@gmail.com>
2022-01-29 20:26:40 +0100

    use the escape character to insert a single quote

--- 9.py
+++ 9.py
@@ -259,3 +259,4 @@ print("I want to pay {2} dollars for {0} pieces of item {1}.".format(3, 567, 49.
 
 # escape character
 print("We are the so-called \"Vikings\" from the north.")
+print('It\'s alright.')
