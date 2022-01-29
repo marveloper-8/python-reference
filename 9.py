f9e4e70d6f76c0b16ba19ea4aa2ea8286b23a0f8
Joshua Equere <marveloper.8@gmail.com>
2022-01-29 21:18:50 +0100

    any valid value returns True

--- 9.py
+++ 9.py
@@ -287,3 +287,15 @@ z = 15
 
 print(bool(y))
 print(bool(z))
+
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool(()))
+# print(bool([]))
+# print(bool({}))
