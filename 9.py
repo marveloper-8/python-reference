644486bd9f39293fae2542615629b4f5d9178671
Joshua Equere <marveloper.8@gmail.com>
2022-02-03 15:19:42 +0100

    check if a string has any 2 digit numbers from 00 to 59

--- 9.py
+++ 9.py
@@ -1285,3 +1285,9 @@ if re.findall("[0-9]", cp):
     print("Yes, there is at least one match!")
 else:
     print("No match!")
+
+print(re.findall("[0-5][0-9]", cp))
+if re.findall("[0-5][0-9]", cp):
+    print("Yes, there is at least one match!")
+else:
+    print("No match")
